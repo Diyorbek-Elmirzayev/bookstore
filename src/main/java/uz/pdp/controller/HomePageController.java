8ab116e49939e8e394875d73534c3922a8920dd5
diyor <diyorelmirzayev2003@gmail.com>
2023-02-12 17:42:31 +0500

    authentification

--- src/main/java/uz/pdp/controller/HomePageController.java
+++ src/main/java/uz/pdp/controller/HomePageController.java
@@ -1,5 +1,14 @@
 package uz.pdp.controller;
 
-public class HomePageController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping
+public class HomePageController {
+    @GetMapping
+    public String homePage(){
+        return "homePage";
+    }
 }
--- src/main/java/uz/pdp/model/Language.java
+++
@@ -1,6 +0,0 @@
-package uz.pdp.model;
-
-public class Language {
-    private Integer id;
-    private String name;
-}
